f07f099396f320189fa94ebd0925d7bfe21b0a84
amzadhossainjacky <amzadhossainjacky@gmail.com>
2020-04-10 17:59:21 +0600

    basic layout created & landing page showed

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>BUS TICKET RESERVATION SYSTEM</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -45,17 +45,18 @@
             }
 
             .title {
-                font-size: 84px;
+                font-size: 64px;
             }
 
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
-                font-size: 13px;
+                font-size: 18px;
                 font-weight: 600;
                 letter-spacing: .1rem;
                 text-decoration: none;
                 text-transform: uppercase;
+                color: chocolate;
             }
 
             .m-b-md {
@@ -81,18 +82,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    BUS TICKET RESERVATION SYSTEM
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs">LOGIN</a>
                 </div>
             </div>
         </div>
